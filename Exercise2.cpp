b40b987700d7f642c99e4c34d20ca27f35e37623
grotedela <goyodl@icloud.com>
2017-02-03 12:21:53 -0600

    Add files via upload

---
+++ Exercise2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+// main: generate some simple output
+
+int main () {
+int nu1, nu2, tot;
+  cout << "This program will add two given numbers." << endl;
+  cout << "Type the first number: " << endl;
+  cin >> (nu1);
+  cout << "Type the second number: " << endl;
+  cin >> (nu2);
+  tot = nu1+nu2;
+  cout << "The result is: "<< tot <<endl;;
+
+return 0;
+
+
+}
