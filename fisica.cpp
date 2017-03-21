b52846d1e4fbdd3ade29a495f19c166c586fb7ec
grotedela <goyodl@icloud.com>
2017-03-21 10:58:41 -0600

    Add files via upload

---
+++ fisica.cpp
@@ -0,0 +1,55 @@
+include <iostream>
+include <cmath>
+
+using namespace std;
+
+main(){
+
+  double i1, j1, k1;
+
+cout << "This program will calculate hsdkhdsjl. \n";
+cout << "Give me the 'I' value \n";
+cin >> i1
+cout << "Give me the 'J' value \n";
+cin >> j1
+cin >> i1cout << "Give me the 'K' value \n";
+cin >> k1
+
+int main()
+{
+  int input;
+
+  cout<<"1. Play game\n";
+  cout<<"2. Load game\n";
+  cout<<"3. Play multiplayer\n";
+  cout<<"4. Exit\n";
+  cout<<"Selection: ";
+  cin>> input;
+  switch ( input ) {
+  case 1:            
+    {int eats = x+y;
+    return eats;
+    }
+    break;
+  case 2:            // Note the colon, not a semicolon
+    loadgame();
+    break;
+  case 3:            // Note the colon, not a semicolon
+    playmultiplayer();
+    break;
+  case 4:            // Note the colon, not a semicolon
+    cout<<"Thank you for playing!\n";
+    break;
+  default:            // Note the colon, not a semicolon
+    cout<<"Error, bad input, quitting\n";
+    break;
+  }
+  cin.get();
+
+
+
+
+
+
+
+}
